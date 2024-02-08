219083cae94f4ed29348e67ede1a29677dc32521
FractionMari <lesteberg@gmail.com>
2024-02-08 21:19:16 +0100

    Playing with tone.js

--- code.js
+++ code.js
@@ -244,7 +244,7 @@ function countNeighbors(row, col) {
     if (row+1 < rows && col+1 < cols) {
         if (grid[row+1][col+1] == 1) count++;
     }
-    console.log(grid);
+    console.log(row);
     return count;
     
 }

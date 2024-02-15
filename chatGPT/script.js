3bb7011f816195ce5c243aaa5e72351f7a11b799
FractionMari <lesteberg@gmail.com>
2024-02-15 19:41:22 +0100

    Creating new polysynth along the way

--- chatGPT/script.js
+++ chatGPT/script.js
@@ -95,16 +95,8 @@ function drawGlider(i, j) {
 
             //var isLive = Math.round(Math.random());
             //console.log(isLive);
-            while (i == 0 && j == 0) {
-
-                const cell = gridContainer.children[i * cols + j];
-                cell.classList.toggle('alive');
-                grid[i][j] = 1;
-                
-
-
-            }
-            while (i == 21 && j == 1) {
+           
+            if (i == 21 && j == 1) {
 
                 const cell = gridContainer.children[i * cols + j];
                 cell.classList.toggle('alive');
